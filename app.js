30aa5cf658ddb5f29b98d9843d6e0e7e55e9462b
Malkaldi <mohammed.alkaldi02@gmail.com>
2024-02-09 20:58:34 +0300

    aws

--- app.js
+++ app.js
@@ -1,16 +1,16 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-app.use(cors({
-    origin: process.env.SERVER
-}));
+
 const path = require('path');
 
 require('dotenv').config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
-
+app.use(cors({
+    origin: process.env.SERVER
+}));
 app.use(cors());
 app.use(express.json());
 app.use(express.static('public')); // Serve static files
